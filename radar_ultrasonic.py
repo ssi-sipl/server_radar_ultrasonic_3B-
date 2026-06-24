b2689458bab358fc47063eb2c264b65be9e80a38
Rikky Yadav <rikkyyadav636@gmail.com>
2026-06-24 12:34:29 +0530

    sensor distance can be changed from terminal command

--- radar_ultrasonic.py
+++ radar_ultrasonic.py
@@ -5,272 +5,431 @@ import time
 import serial
 import threading
 import logging
-import pigpio
-import sys
+import os
 
-pi = pigpio.pi()
-if not pi.connected:
-    raise RuntimeError("pigpio daemon not working")
+# Logging
 
-# Configure logging
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-# GPIO Configuration
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
+#lock for send_alert 
+alert_lock = threading.Lock()
 
-#Enable / Logic-high pin
-ENABLE_PIN = 15
-GPIO.setup(ENABLE_PIN, GPIO.OUT)
-GPIO.output(ENABLE_PIN, GPIO.HIGH)
+# GPIO Mode
+# Using BCM numbering because power pins use GPIO numbers.
+# If you prefer BOARD numbering, change the pin numbers
+# accordingly.
 
-RPiGPIO_PINS = [23, 24, 13, 15]
-for pin in RPiGPIO_PINS:
-    GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setmode(GPIO.BCM)
 
-pgio_pins = [17,23]
-for pin in pgio_pins:
-    pi.set_mode(pin, pigpio.INPUT)
+# AI Box URL
+#change the ip according to the AI Box ip address
 
-pi.set_mode(5, pigpio.OUTPUT)
-pi.set_mode(5,0)
+SERVER_URL = "http://192.168.1.100:5000/api/alerts/from-nx"
 
-ULTRASONIC_SENSORS = [                      # Hardcoded GPIO pin for trigger and echo for each ultrasonic sensor
-    {"id": "US1", "trig": 23, "echo": 24},
-    {"id": "US2", "trig": 15, "echo": 13},
-]
+# Configuration File
 
-for sensor in ULTRASONIC_SENSORS:
-    GPIO.setup(sensor["trig"], GPIO.OUT)
-    GPIO.setup(sensor["echo"], GPIO.IN)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# Radar sensor serial communication setup
-RADAR_PORT = '/dev/ttyS0'  # Hardcoded serial port for radar sensor
-RADAR_BAUDRATE = 9600        # Hardcoded baud rate for radar sensor
+CONFIG_FILE = os.path.join(BASE_DIR, "sensors.json")
 
-# Server configuration
-SERVER_URL = 'http://192.168.1.5:5000/api/alerts/from-nx'  # URL for testing on local server
+# Sensor Definitions
 
-# Default sensor ranges (per sensor)
-SENSOR_RANGES = {
-    "US1": {"min": 120, "max": 780},
-    "US2": {"min": 120, "max": 780},
-    "RADAR_1": {"min": 120, "max": 780},
-    "RADAR_2": {"min": 120, "max": 780}
+SENSORS = {
+
+    "RD001": {
+        "type": "radar",
+        "uart": "/dev/ttyS0",
+        "baudrate": 115200,
+        "power_pin": 17
+    },
+
+    "US001": {
+        "type": "ultrasonic",
+        "trig": 23,
+        "echo": 24,
+        "power_pin": 27
+    }
+
 }
 
+# GPIO Setup
 
-# Read command-line arguments per sensor
-for arg in sys.argv[1:]:
-    if "=" in arg:
-        key, value = arg.split("=")
+for sensor in SENSORS.values():
 
-        # Split only last underscore
-        if "_" in key:
-            sensor_name, limit_type = key.rsplit("_", 1)
+    GPIO.setup(sensor["power_pin"], GPIO.OUT)
 
-            if sensor_name in SENSOR_RANGES and limit_type in ["min", "max"]:
-                SENSOR_RANGES[sensor_name][limit_type] = int(value)
+    GPIO.output(sensor["power_pin"], GPIO.HIGH)
 
-print("Active Sensor Ranges:")
-for sensor, ranges in SENSOR_RANGES.items():
-    print(f"{sensor} -> {ranges['min']} - {ranges['max']}")
+    if sensor["type"] == "ultrasonic":
+
+        GPIO.setup(sensor["trig"], GPIO.OUT)
+        GPIO.setup(sensor["echo"], GPIO.IN)
+
+# Power Control
+
+def sensor_on(power_pin):
+
+    GPIO.output(power_pin, GPIO.LOW)
 
 
-# Function to send HTTP command
-def send_http_command(url, method='POST', params=None, data=None, headers=None):
+def sensor_off(power_pin):
+
+    GPIO.output(power_pin, GPIO.HIGH)
+
+
+# Configuration Loader
+
+def load_config():
+
+    if not os.path.exists(CONFIG_FILE):
+
+        default = {
+
+            "sensorBoxId":"sensor1",
+
+            "RD001": {
+                "enabled": True,
+                "min_range": 120,
+                "max_range": 400
+            },
+
+            "US001": {
+                "enabled": True,
+                "min_range": 120,
+                "max_range": 400
+            }
+
+        }
+
+        with open(CONFIG_FILE, "w") as f:
+             json.dump(default, f, indent=4)
+
+        return default
+
+    with open(CONFIG_FILE, "r") as f:
+
+        return json.load(f)
+    
+# HTTP Communication
+
+def send_http_command(payload):
+
     try:
-        response = requests.request(method, url, params=params, data=data, headers=headers, timeout=0.5)
-        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
-        return response.text
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return None
 
-def measure_distance_ultrasonic(trig, echo):
+        response = requests.post(
+            SERVER_URL,
+            json=payload,
+            timeout=5
+        )
+
+        response.raise_for_status()
+
+        logging.info(
+            f"Alert sent successfully ({payload['sensorId']})"
+        )
+
+    except Exception as e:
+
+        logging.error(f"HTTP Error : {e}")
+
+# Alert Generator
+
+def send_alert(sensor_id):
+
+    with alert_lock:
+        timestamp_us = int(time.time() * 1000000)
+        config = load_config()
+        sensor_box_id = config["sensorBoxId"]
+        
+        payload = {
+
+        "sensorId": "sensorBoxId",
+
+        "data":"Type:nx.base.Detection;Confidence:0.72;TimestampUs:{timestamp_us};"
+        }
+        send_http_command(payload)
+        time.sleep(3)
+
+
+# Ultrasonic Distance Function
+
+def measure_distance(trig, echo):
+
     GPIO.output(trig, GPIO.HIGH)
-    time.sleep(0.00001)  # 10 microseconds
+    time.sleep(0.00001)
     GPIO.output(trig, GPIO.LOW)
 
-    start_time = time.time()
-    timeout = 0.02
-    while GPIO.input(echo) == GPIO.LOW:
-        pulse_start = time.time()
-        if pulse_start - start_time > timeout:  # Timeout for no echo received
-            return -1
     pulse_start = time.time()
+    timeout = pulse_start
 
-    while GPIO.input(echo) == GPIO.HIGH:
-        pulse_end = time.time()
-        if pulse_end - pulse_start > timeout:  # Timeout for long echo
+    while GPIO.input(echo) == 0:
+
+        pulse_start = time.time()
+
+        if pulse_start - timeout > 0.02:
+
             return -1
 
-    pulse_duration = pulse_end - pulse_start
-    distance = pulse_duration * 17150  # Convert to cm
+    while GPIO.input(echo) == 1:
+
+        pulse_end = time.time()
+
+        if pulse_end - pulse_start > 0.02:
+            
+            return -1
+
+    duration = pulse_end - pulse_start
+
+    distance = duration * 17150
 
     if distance < 2 or distance > 800:
+
         return -1
 
     return distance
 
-# Function to read from the radar sensor (UART)
-def read_from_port(ser):
-    try:
-        while True:
-            try:
-                if ser.in_waiting > 0:
-                    data = ser.readline(ser.in_waiting)
-                    # Decode incoming bytes
-                    decoded_data = data.decode("utf-8", errors="ignore").strip()
+# Radar Worker
 
-                    # Process complete lines only
-                    numeric_values = "".join(filter(str.isdigit, decoded_data))
-                    if numeric_values:
-                        distance = float(numeric_values)
-                        if VALID_RANGE_MIN <= distance <= VALID_RANGE_MAX:
-                            logging.info(f"RADAR_1 |Radar Sensor Value: {distance} cm")
-                            check_and_send_request(distance, "RADAR_1", "Hardware_UART")
-                        else:
-                            logging.info(f"RADAR1 | Distance {distance:.2f} cm is out of the valid range (10 - 780 cm).")
-                time.sleep(0.1)  # Sleep briefly to avoid busy waiting
-            except serial.SerialException as e:
-                logging.error(f"Serial error: {e}")
-                time.sleep(1)
-    except KeyboardInterrupt:
-        logging.info("Exiting...")
-    finally:
-        ser.close()  # Ensure the serial port is closed on exit
+def radar_worker(sensor_id):
 
-def read_from_soft_uart(rx_gpio, tx_gpio, sensor_id):
-    BAUD = 9600
-    buffer = ""
+    logging.info(f"radar working is started {"sensor_id"}")
+    sensor = SENSORS[sensor_id]
 
-    pi.set_mode(rx_gpio, pigpio.INPUT)
-    pi.set_mode(tx_gpio, pigpio.OUTPUT)
-    pi.write(tx_gpio, 1)
+    while True:
 
-    # Ensure GPIO is not already in use
+        config = load_config()
 
-    try:
-        pi.bb_serial_read_open(rx_gpio, BAUD)
-        logging.info(f"Software UART radar started for sensor {sensor_id} on GPIO {rx_gpio}"
-        f"(RX={rx_gpio}, TX={tx_gpio})")
+        sensor_config = config["sensors"].get(sensor_id, {})
 
-        while True:
-            count, data = pi.bb_serial_read(rx_gpio)
-            if count > 0:
-                buffer += data.decode("utf-8", errors="ignore")
+        if not sensor_config.get("enabled", False):
 
-                while "\n" in buffer:
-                    line, buffer = buffer.split("\n", 1)
-                    line = line.strip()
+            sensor_off(sensor["power_pin"])
 
-                    if line.isdigit():
-                        distance = int(line)
-                        if VALID_RANGE_MIN <= distance <= VALID_RANGE_MAX:
-                            logging.info(f"{sensor_id} | Radar Distance: {distance} cm")
-                            check_and_send_request(distance, sensor_id, "Software_UART")
-                        else:
-                            logging.info(
-                                f"{sensor_id} | Distance {distance} cm out of range"
-                            )
+            time.sleep(2)
 
-            time.sleep(0.05)
-    except Exception as e:
-        logging.error(f"{sensor} | Software UART error: {e}")
-    finally:
-        pi.bb_serial_read_close(rx_gpio)
-        logging.info(f"{sensor_id} | Software UART stopped")
+            continue
 
-
-def check_and_send_request(distance_cm, sensor_id, sensor_type):
-
-    sensor_min = SENSOR_RANGES[sensor_id]["min"]
-    sensor_max = SENSOR_RANGES[sensor_id]["max"]
-
-    if sensor_min <= distance_cm <= sensor_max:
-
-
-        # Convert cm → meters
-        distance_m = distance_cm / 100.0
-
-        # Create formatted timestamp string
-        time_stamp_str = time.strftime("%Y-%m-%d %H:%M:%S")
-
-        # Convert to microseconds
-        timestamp_seconds = int(
-            time.mktime(
-                time.strptime(time_stamp_str, "%Y-%m-%d %H:%M:%S")
-            )
-        )
-        timestamp_us = timestamp_seconds * 1_000_000
-
-        data_string = (
-            f"Type:nx.base.Sensor;"
-            f"distance:{distance_m};"
-            f"TimestampUs:{timestamp_us};"
-        )
-
-        payload = {
-            "sensorId": "ipcam159",
-            "data": data_string
-        }
-
-        headers = {'Content-Type': 'application/json'}
+        sensor_on(sensor["power_pin"])
 
         try:
-            requests.post(SERVER_URL, json=payload, headers=headers)
 
-            logging.info(
-                f"{sensor_id} ({sensor_type}) → Sent NX event | {time_stamp_str}"
-            )
+            with serial.Serial(
+                sensor["uart"],
+                baudrate=sensor["baudrate"],
+                timeout=1
+            ) as ser:
 
+                logging.info(
+                    f"{sensor_id} connected on "
+                    f"{sensor['uart']}"
+                )
+
+                while True:
+                     config = load_config()
+
+                     sensor_config = config["sensors"].get(
+                        sensor_id,
+                        {}
+                     )
+
+                     if not sensor_config.get("enabled",False):
+                        break
+
+                     min_range = sensor_config.get(
+                        "min_range",
+                        120
+                     )
+
+                     max_range = sensor_config.get(
+                        "max_range",
+                        400
+                     )
+
+                     logging.info(f"Bytes waiting: {ser.in_waiting}")
+                     if ser.in_waiting:
+                        data = ser.read(ser.in_waiting)
+
+                        try:
+                            text = data.decode("utf-8", errors="ignore")
+
+                            for line in text.splitlines():
+
+                                line = line.strip()
+
+                                if not line:
+                                    continue
+
+                                if line.startswith("Range"):
+
+                                     parts = line.split()
+
+                                     if len(parts) >= 2:
+
+                                         distance = float(parts[1])
+
+                                         logging.info(f"{sensor_id} Distance={distance}")
+
+                                         if min_range <= distance <= max_range:
+                                             
+                                             logging.info(
+                                                 f"{sensor_id} DETECTED {distance}"
+                                             )
+
+                                             send_alert(...)
+                                             time.sleep(3)
+                        except Exception as e:
+                            logging.error(e)
         except Exception as e:
-            logging.error(f"HTTP Error: {e}")
 
-    else:
-            logging.info(
-                f"{sensor_id} ({sensor_type}) | Distance {distance_cm:.2f} cm "
-                f"out of range ({sensor_min}-{sensor_max})"
+            logging.error(
+                f"{sensor_id} Error : {e}"
             )
 
+            time.sleep(0.5)
+
+# Ultrasonic Worker
+
+def ultrasonic_worker(sensor_id):
+
+    logging.info(f"ultrasonic worker started with {"sensor_id"}")
+    sensor = SENSORS[sensor_id]
+
+    while True:
+
+        config = load_config()
+
+        sensor_config = config["sensors"].get(
+            sensor_id,
+            {}
+        )
+
+        if not sensor_config.get(
+            "enabled",
+            False
+        ):
+
+            sensor_off(
+                sensor["power_pin"]
+            )
+            
+            time.sleep(2)
+
+            continue
+
+        sensor_on(
+            sensor["power_pin"]
+        )
+
+        min_range = sensor_config.get(
+            "min_range",
+            120
+        )
+
+        max_range = sensor_config.get(
+            "max_range",
+            400
+        )
+
+        distance = measure_distance(
+            sensor["trig"],
+            sensor["echo"]
+        )
+
+        if distance != -1:
+            logging.info(
+                f"{sensor_id}"
+                f" Distance="
+                f"{distance:.2f}"
+            )
+
+            if (min_range<= distance<= max_range):
+
+                logging.info(
+                    f"{sensor_id}"f" DETECTED"f"{distance}"
+                )
+
+                send_alert(sensor_id)
+                time.sleep(3)
+
+# Thread Manager
+
+def start_sensor_threads():
+
+    threads = []
+
+    for sensor_id, sensor in SENSORS.items():
+
+        if sensor["type"] == "ultrasonic":
+
+            thread = threading.Thread(
+                target=ultrasonic_worker,
+                args=(sensor_id,)
+            )
+
+        elif sensor["type"] == "radar":
+
+            thread = threading.Thread(
+                target=radar_worker,
+                args=(sensor_id,)
+            )
+
+        else:
+
+            continue
+        thread.daemon = True
+
+        thread.start()
+
+        threads.append(thread)
+
+        logging.info(
+            f"Started thread "
+            f"{sensor_id}"
+        )
+
+    return threads
+                
+# Main
 
 def main():
-    try: # Hardware UART radar
-        hw_ser = serial.Serial("/dev/serial0", baudrate=RADAR_BAUDRATE, timeout=1)
-        logging.info("Hardware UART radar connected on /dev/serial0")
 
-        hw_thread = threading.Thread(target=read_from_port, args=(hw_ser,), daemon=True)
-        hw_thread.start()
+    logging.info(
+        "Sensor Manager Started"
+    )
 
+    start_sensor_threads()
 
-        # Software UART radar (RADAR_2); GPIO17 → Physical pin 11
-        sw_thread_1 = threading.Thread(target=read_from_soft_uart,args=(17, 5, "RADAR_2"),daemon=True)
-        sw_thread_1.start()
+    while True:
 
-        # Software UART radar (RADAR_3); GPIO27 → Physical pin 13
-        #sw_thread_2 = threading.Thread(dtarget=read_from_soft_uart,args=(17, "RADAR_3"),daemon=True)
-        #sw_thread_2.start()
+        time.sleep(10)
 
-        # Start the ultrasonic sensor reading loop in the main thread
-        while True:
-            for sensor in ULTRASONIC_SENSORS:
-                distance = measure_distance_ultrasonic(sensor["trig"], sensor["echo"])
-                if distance != -1:
-                    logging.info(f"{sensor['id']} Ultrasonic Distance: {distance:.2f} cm")
-                    check_and_send_request(distance, sensor["id"], "Ultrasonic")
-            time.sleep(3)
-
-    except KeyboardInterrupt:
-        logging.info("Program interrupted by user.")
-    finally:
-        try:
-            hw_ser.close()
-        except Exception:
-            pass
-        GPIO.cleanup()
-        pi.stop()
-        logging.info("GPIO cleaned up. Exiting program.")
+# Entry Point
 
 if __name__ == "__main__":
-    main()
+
+    try:
+
+        main()
+
+    except KeyboardInterrupt:
+        
+        logging.info(
+            "Stopping Sensor Manager"
+        )
+
+    finally:
+
+        for sensor in SENSORS.values():
+
+            sensor_off(
+                sensor["power_pin"]
+            )
+
+        GPIO.cleanup()
+
+        logging.info(
+            "GPIO Cleaned"
+        )
